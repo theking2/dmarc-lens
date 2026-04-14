74f78ba3c3294c8f701e958c23e24ebdfdecda4a
Johannes Kingma <theking2@king.ma>
2026-04-14 21:36:50 +0200

    Update mail server configuration in config_sample.php

---
+++ config_sample.php
@@ -0,0 +1,6 @@
+<?php
+// Copy this file to config.php and fill in your mail server details
+
+define( 'MAILBOX', '{exacmple.domain.com:993/imap/ssl}INBOX');
+define( 'MAILUSERNAME', 'username');
+define( 'MAILPASSWORD', 'password');
